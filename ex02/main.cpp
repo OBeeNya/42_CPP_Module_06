adea98d76e351fb6112411ee27bd02f2814d982f
OBeeNya <benjamin.aubignat@gmail.com>
2022-10-14 08:34:04 +0200

    ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -31,27 +31,15 @@ Base	*generate(void)
 void	identify(Base *p)
 {
 	std::cout << "Identified type: ";
-	try
-	{
-		A	*a = dynamic_cast<A *>(p);
-		if (a)
-			std::cout << "A" << std::endl;
-	}
-	catch (std::bad_cast &e) {}
-	try
-	{
-		B	*b = dynamic_cast<B *>(p);
-		if (b)
-			std::cout << "B" << std::endl;
-	}
-	catch (std::bad_cast &e) {}
-	try
-	{
-		C	*c = dynamic_cast<C *>(p);
-		if (c)
-			std::cout << "C" << std::endl;
-	}
-	catch (std::bad_cast &e) {}
+	A	*a = dynamic_cast<A *>(p);
+	if (a)
+		std::cout << "A" << std::endl;
+	B	*b = dynamic_cast<B *>(p);
+	if (b)
+		std::cout << "B" << std::endl;
+	C	*c = dynamic_cast<C *>(p);
+	if (c)
+		std::cout << "C" << std::endl;
 }
 
 void	identify(Base &p)
